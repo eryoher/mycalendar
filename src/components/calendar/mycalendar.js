a9a20d017dfe9668bf8b0d3f3ac37e3247b8272f
eryoher <eryoher@gmail.com>
2020-12-20 01:41:03 -0500

    day one

--- src/components/calendar/mycalendar.js
+++ src/components/calendar/mycalendar.js
@@ -1,7 +1,86 @@
 import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import moment from "moment";
 
 export default class mycalendar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentMonth: moment(),
+			weekdays: moment.weekdays(), //["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"]
+		};
+	}
+
+	renderWeeks = () => {
+		const { weekdays } = this.state;
+		const results = [];
+
+		weekdays.forEach((day) => {
+			results.push(<th key={day}>{day}</th>);
+		});
+
+		return results;
+	};
+
+	renderDays = () => {
+		const { currentMonth } = this.state;
+		const results = [],
+			rows = [],
+			blanks = [];
+
+		const daysInMonth = currentMonth.daysInMonth();
+		const firstDay = currentMonth.startOf("month").format("d");
+
+		let cells = [];
+
+		for (let i = 0; i < firstDay; i++) {
+			blanks.push(<td key={i + 31}>{""}</td>);
+		}
+
+		for (let i = 1; i <= daysInMonth; i++) {
+			const day = (
+				<td key={i} className={"calendar-day"}>
+					<span>{i}</span>
+				</td>
+			);
+
+			results.push(day);
+		}
+
+		var totalSlots = [...blanks, ...results];
+
+		totalSlots.forEach((row, i) => {
+			if (i % 7 !== 0) {
+				cells.push(row);
+			} else {
+				let insertRow = cells.slice();
+				rows.push(insertRow);
+				cells = [];
+				cells.push(row);
+			}
+			if (i === totalSlots.length - 1) {
+				let insertRow = cells.slice();
+				rows.push(insertRow);
+			}
+		});
+
+		const trElems = rows.map((d, i) => {
+			return <tr key={i * 100}>{d}</tr>;
+		});
+
+		return trElems;
+	};
+
 	render() {
-		return <div>{"aca el calendario"}</div>;
+		return (
+			<Container className={"text-center"}>
+				<table className={"table table-bordered"}>
+					<thead>
+						<tr className='calendar-header'>{this.renderWeeks()}</tr>
+					</thead>
+					<tbody>{this.renderDays()}</tbody>
+				</table>
+			</Container>
+		);
 	}
 }
